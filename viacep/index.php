ca62de244986ad3b492c6a19818e593886616dc8
Guilherme Luciano Donin Villaca <guilherme.villaca@docente.pr.senac.br>
2024-07-25 22:21:46 -0300

    atividade viacep

---
+++ viacep/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VIA CEP</title>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>VIA CEP</h1>
+
+    <form>
+
+        <input type="text" name="cep" id="cep" placeholder="CEP" onblur="buscaCep()">
+        <br>
+        <input type="text" name="logradouro" id="logradouro" placeholder="Logradouro">        
+        <br>
+        <input type="text" name="numero" id="numero" placeholder="Número">
+        <br>
+        <input type="text" name="complemento" id="complemento" placeholder="Complemento">
+        <br>
+        <input type="text" name="bairro" id="bairro" placeholder="Bairro">
+        <br>
+        <input type="text" name="uf" id="uf" placeholder="UF">
+        <br>
+        <input type="text" name="localidade" id="localidade" placeholder="localidade">
+        <br>
+        <input type="text" name="ibge" id="ibge" placeholder="IBGE">
+        <br>
+        <input type="text" name="ddd" id="ddd" placeholder="DDD">        
+
+    </form>
+
+</body>
+</html>
